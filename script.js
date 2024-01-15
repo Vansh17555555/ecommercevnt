b6eb0f0cbb1b364b72c49470c641b6004445aaad
Vansh Verma <vvansh739@gmail.com>
2024-01-16 00:24:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,75 @@ if(bar){
         nav.classList.remove('active');
     })
 }
+function submitSignInForm(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const formData = {
+        username: username,
+        password: password
+    };
+
+    fetch('http://localhost:2000/signin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert("🎉 Logged in with love! 🥰");
+        window.location.replace(window.location.origin + '/Build-and-Deploy-Ecommerce-Website/index.html');
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+const signInForm = document.getElementById('signin-form');
+if (signInForm) {
+    signInForm.addEventListener('submit',submitSignInForm);
+    };
+
+
+
+function submitSignUpForm(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const formData = {
+        username: username,
+        password: password
+    };
+
+    fetch('http://localhost:2000/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert("🎉 Signed up successfully!");
+
+        // Use an absolute path for redirection
+        window.location.replace(window.location.origin + '/Build-and-Deploy-Ecommerce-Website/index.html');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert("Error during signup. Please try again.");
+    });
+}
+
+const signUpForm = document.getElementById('signup-form');
+if (signUpForm) {
+    signUpForm.addEventListener('submit', submitSignUpForm);
+}
